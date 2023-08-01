6970c006467b54ea66eb3f514bf61cdb82dc5a9f
Andrei <yeloww51@gmail.com>
2023-08-01 22:29:21 +0300

    simple CRUD

--- bboom_tz/blog/serializers.py
+++ bboom_tz/blog/serializers.py
@@ -1,18 +1,27 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, CharField, IntegerField, Serializer
+
 from .models import User, Post
 
 
 class UserSerializer(ModelSerializer):
-
-
     class Meta:
         model = User
         fields = ('name', 'email')
 
 
-class PostSerializer(ModelSerializer):
+class PostSerializer(Serializer):
+    title = CharField(max_length=100)
+    body = CharField(max_length=500)
+    author_id = IntegerField()
 
+    def create(self, validated_data: dict) -> Post:
+        return Post.objects.create(**validated_data)
 
-    class Meta:
-        model = Post
-        field = ('title', 'body', 'author')
+    def update(self, instance: Post, validated_data: dict):
+        instance.title = validated_data.get("title", instance.title)
+        instance.body = validated_data.get("body", instance.body)
+        instance.author_id = validated_data.get("author_id", instance.author_id)
+        instance.save()
+        print(type(instance))
+        return instance
+
--- bboom_tz/blog/views.py
+++ bboom_tz/blog/views.py
@@ -1,15 +1,57 @@
-from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import User, Post
 from .serializers import UserSerializer, PostSerializer
 
 
-class UserAPIView(ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class UserAPIView(APIView):
+    def get(self, request: Request) -> Response:
+        users_list = User.objects.all()
+        serializer = UserSerializer(users_list, many=True)
+        return Response(status=200, data={"users": serializer.data})
 
 
+class UserPostsAPIView(APIView):
 
-class PostAPIView(ListAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    def get(self, request: Request, user_id: int) -> Response:
+        user_posts = Post.objects.filter(author_id=user_id).all()
+        serializer = PostSerializer(user_posts, many=True)
+        return Response(status=200, data={"user posts": serializer.data})
+
+
+class PostAPIView(APIView):
+    def get(self, request: Request) -> Response:
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(status=200, data={"posts": serializer.data})
+
+    def post(self, request: Request) -> Response:
+        serializer = PostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(status=201, data={"post info": serializer.data})
+
+    def put(self, request: Request, post_id: int) -> Response:
+        try:
+            instance = Post.objects.get(id=post_id)
+        except Post.DoesNotExist as e:
+            return Response(status=404, data={"error": "Object does not exists"})
+
+        serializer = PostSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(status=200, data={"updated post": serializer.data})
+
+    def delete(self, request: Request, post_id: int) -> Response:
+        try:
+            instance = Post.objects.get(id=post_id)
+        except Post.DoesNotExist as e:
+            return Response(status=404, data={"error": "Object does not exists"})
+
+        instance.delete()
+
+        return Response(status=204)
